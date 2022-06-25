ec46e28e75e9ffb6f062d7cd41b9c601435b735a
jatin <ajagrawal412@gmail.com>
2022-06-25 15:49:44 +0530

    ok

--- header.php
+++ header.php
@@ -7,4 +7,4 @@
   <title>wow chat</title>
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-</head>
+</head> 
